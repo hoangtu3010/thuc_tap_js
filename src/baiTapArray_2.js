16c0b0d0be25b342aa20bbc9014373fe66e4ab4d
hoangtu3010 <70878927+hoangtu3010@users.noreply.github.com>
2021-01-12 16:49:03 +0700

    baiTapArray

---
+++ src/baiTapArray_2.js
@@ -0,0 +1,36 @@
+//In ra các phần tử âm của mảng
+
+function negative(array) {
+  for (let i = 0; i < array.length; i++) {
+    const soAm = array[i];
+    if (soAm < 0) console.log(soAm);
+  }
+}
+negative([2, 1, -1, -2, 3]);
+
+//Tách 1 mảng thành 2 mảng chăn và lẻ
+
+function chanLe(mang) {
+  let ketQua = {
+    mangChan: [],
+    mangLe: []
+  };
+  mang.forEach(function (item, index) {
+    if (item % 2 == 0) ketQua.mangChan.push(item);
+    else ketQua.mangLe.push(item);
+  });
+  return ketQua;
+}
+console.log(chanLe([1, 2, 3, 4, 5]));
+
+//Lấy ra các phần tử chẵn và nhỏ hơn 20
+function layMangChan(mang) {
+  let ketQua = [];
+  mang.forEach(function (item, index) {
+    if (item % 2 == 0 && item < 20) ketQua.push(item);
+  });
+  return ketQua;
+}
+console.log(layMangChan([1, 2, 3, 4, 5, 21, 22]));
+
+//
